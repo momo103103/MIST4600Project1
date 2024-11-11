455bdfc9a69d7bcc416cb5ccc67b9887926edf87
momo103103 <mohammed3103omar@gmail.com>
2024-11-11 12:39:18 -0500

    Update LoginManager.java - Mohammed
    Updated Prompts

--- LoginManager.java
+++ LoginManager.java
@@ -1,59 +1,52 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class LoginManager {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        AccountManager accountManager = new AccountManager(); 
+        boolean done = true;
 
-		Scanner input = new Scanner(System.in);
-		
-		
-		boolean done = false;
-		while(!done)
-		{
-			
-		
-		System.out.println("Welcome to Java Project Bank Account");
-		System.out.println("What would you like to do today?");
-		System.out.println("1: Create an account");
-		System.out.println("2: Login into your account");
-		System.out.println("3: Exit the program");
-		
-		System.out.println("Please enter your choice (1-3)");
-		int option = input.nextInt();
-		
-		input.nextLine();
-		
-		switch(option) {
-		
-		case 1:
-			
-			
-			
-			break;
-			
-			
-		case 2:
-			
-			
-			
-			break;
-			
-		case 3:
-			done = true;
-			System.out.println("Thank you for using the system.");
-			
-			break;
-			
-			default:
-			
-			break;
-		
+        System.out.println("Welcome to Java Project Bank Account");
 
-		
-		}
+        while (done) {
+            System.out.println("What would you like to do today?");
+            System.out.println("1: Create an account");
+            System.out.println("2: Log into your account");
+            System.out.println("3: Exit the program");
+            System.out.print("Please enter your choice (1-3): ");
+            int option = input.nextInt();
+            input.nextLine(); 
 
-		
-	}
-	}
+            switch (option) {
+                case 1:
+                    System.out.print("Enter username for new account: ");
+                    String newUsername = input.nextLine();
+                    System.out.print("Enter password for new account: ");
+                    String newPassword = input.nextLine();
+                    accountManager.registerAccount(newUsername, newPassword);
+                    break;
+
+                case 2:
+                    System.out.print("Enter username: ");
+                    String username = input.nextLine();
+                    System.out.print("Enter password: ");
+                    String password = input.nextLine();
+                    accountManager.login(username, password);
+                    break;
+
+                case 3:
+                    System.out.println("Exiting the program...");
+                    done = false;
+                    break;
+
+                default:
+                    System.out.println("Invalid option. Please try again.");
+                    break;
+            }
+        }
+
+
+        System.out.println("Thank you for using Java Project Bank Account!");
+    }
 }
